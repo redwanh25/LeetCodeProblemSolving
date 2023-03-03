e2b1843c6ef2a8c8fb20a1fa32ce8b6406285d94
Redwan Hossain <redwanh25@gmail.com>
2023-03-03 23:39:59 +0600

    updated

---
+++ LeetCodeProblemSolving/Easy/P_953_Special.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblemSolving.Easy
+{
+    internal class P_953_Special
+    {
+        public bool IsAlienSorted(string[] words, string order)
+        {
+            var alphabet = Enumerable.Range(0, 26).ToDictionary(i => order[i], i => (char)('a' + i));
+            //foreach (var word in alphabet.Keys)
+            //    Console.WriteLine(word + " " + alphabet[word]);
+
+            var translated = words.Select(w => string.Concat(w.Select(c => alphabet[c])));
+            //foreach (var word in translated)
+            //    Console.WriteLine(word);
+
+            return translated.OrderBy(x => x).SequenceEqual(translated);
+        }
+    }
+}
+
+//https://leetcode.com/problems/verifying-an-alien-dictionary/solutions/3130961/short-and-easy-three-lines-of-linq-with-explanation/
--- LeetCodeProblemSolving/MainClass.cs
+++ LeetCodeProblemSolving/MainClass.cs
@@ -7,15 +7,16 @@ namespace LeetCodeProblemSolving
     {
         public static void Main(string[] args)
         {
-            P_1309 obj = new P_1309();
+            P_953_Special obj = new P_953_Special();
 
             int[] arr2 = new int[] { 0, 1, 0, 3, 12 };
             char[] arr1 = new char[] { 'a', 'a', 'b', 'b', 'c', 'c', 'c' };
             int[][] arr3 = new int[][] { new int[] { 1, 5, 2 }, new int[] { 7, 3, 2 }, new int[] { 3, 5, 2 } };
-            string str1 = "10#11#12", str2 = "pq";
+            string[] arr4 = new string[] { "word", "world", "row" };
+            string str1 = "hlabcdefgijkmnopqrstuvwxyz", str2 = "pq";
             int v1 = 1, v2 = 4;
 
-            var res = obj.FreqAlphabets(str1);
+            var res = obj.IsAlienSorted(arr4, str1);
             Console.WriteLine(res);
 
             //obj.MoveZeroes(arr1);
