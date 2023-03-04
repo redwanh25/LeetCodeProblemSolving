e0fc35eab8259b7fcc0a86f250ae2118f164baa7
Redwan Hossain <redwanh25@gmail.com>
2023-03-04 14:38:38 +0600

    updated

---
+++ LeetCodeProblemSolving/Hard/P_2444.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblemSolving.Hard
+{
+    internal class P_2444
+    {
+        public long CountSubarrays(int[] nums, int minK, int maxK)
+        {
+            long answer = 0;
+            // minPos, maxPos : the most recent positions of minK and maxK.
+            // leftBound      : the most recent value outside the range [minK, maxK].
+            int minPos = -1, maxPos = -1, leftBound = -1;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                // If the number is outside the range [minK, maxK], update the most recent leftBound.
+                if (nums[i] < minK || nums[i] > maxK) leftBound = i;
+
+                // If the number is minK or maxK, update the most recent position.
+                if (nums[i] == minK) minPos = i;
+                if (nums[i] == maxK) maxPos = i;
+
+                // Number of valid subarrays == number of elements between leftBound and the smaller of the two most recent minPos and maxPos.
+                answer += Math.Max(0, Math.Min(minPos, maxPos) - leftBound);
+            }
+
+            return answer;
+        }
+    }
+}
--- LeetCodeProblemSolving/MainClass.cs
+++ LeetCodeProblemSolving/MainClass.cs
@@ -1,4 +1,5 @@
 ﻿using LeetCodeProblemSolving.Easy;
+using LeetCodeProblemSolving.Hard;
 using LeetCodeProblemSolving.Medium;
 
 namespace LeetCodeProblemSolving
@@ -7,16 +8,16 @@ namespace LeetCodeProblemSolving
     {
         public static void Main(string[] args)
         {
-            P_953_Special obj = new P_953_Special();
+            P_2444 obj = new P_2444();
 
-            int[] arr2 = new int[] { 0, 1, 0, 3, 12 };
+            int[] arr2 = new int[] { 1, 3, 5, 2, 1, 5 };
             char[] arr1 = new char[] { 'a', 'a', 'b', 'b', 'c', 'c', 'c' };
             int[][] arr3 = new int[][] { new int[] { 1, 5, 2 }, new int[] { 7, 3, 2 }, new int[] { 3, 5, 2 } };
             string[] arr4 = new string[] { "word", "world", "row" };
             string str1 = "hlabcdefgijkmnopqrstuvwxyz", str2 = "pq";
-            int v1 = 1, v2 = 4;
+            int v1 = 1, v2 = 5;
 
-            var res = obj.IsAlienSorted(arr4, str1);
+            var res = obj.CountSubarrays(arr2, v1, v2);
             Console.WriteLine(res);
 
             //obj.MoveZeroes(arr1);
