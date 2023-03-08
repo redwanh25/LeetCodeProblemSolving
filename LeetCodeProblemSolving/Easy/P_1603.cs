17d41b2bf1a4ec99050f82e3cb10f8378d44bb49
Redwan Hossain <redwanh25@gmail.com>
2023-03-08 12:35:20 +0600

    updated

---
+++ LeetCodeProblemSolving/Easy/P_1603.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblemSolving.Easy
+{
+    public class ParkingSystem
+    {
+        private int[] _parkingSpaces;
+
+        public ParkingSystem(int big, int medium, int small)
+        {
+            _parkingSpaces = new int[] { 0, big, medium, small };
+        }
+
+        public bool AddCar(int carType)
+        {
+            if (_parkingSpaces[carType] <= 0)
+                return false;
+            else
+            {
+                _parkingSpaces[carType]--;
+                return true;
+            }
+        }
+    }
+
+    internal class P_1603
+    {
+    }
+}
---
+++ LeetCodeProblemSolving/Easy/P_303.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblemSolving.Easy
+{
+    public class NumArray
+    {
+        int[] _nums;
+        public NumArray(int[] nums)
+        {
+            _nums = nums;
+        }
+
+        public int SumRange(int left, int right)
+        {
+            int rs = 0;
+            for (int i = left; i <= right; i++)
+            {
+                rs += _nums[i];
+            }
+            return rs;
+        }
+    }
+
+    internal class P_303
+    {
+    }
+}
