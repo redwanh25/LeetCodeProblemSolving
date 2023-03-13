575832f5571f090e1c9e868cde235fc0b6943a8b
Redwan Hossain <redwanh25@gmail.com>
2023-03-14 00:11:27 +0600

    updated

---
+++ LeetCodeProblemSolving/Easy/P_101.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblemSolving.Easy
+{
+    internal class P_101
+    {
+        bool Dfs(TreeNode root1, TreeNode root2)
+        {
+            if (root1 is null && root2 is null)
+                return true;
+
+            if (root1?.val != root2?.val)
+                return false;
+
+            return Dfs(root1?.left, root2?.right) && Dfs(root1?.right, root2?.left);
+        }
+
+        public bool IsSymmetric(TreeNode root)
+        {
+            return Dfs(root.left, root.right);
+        }
+    }
+}
